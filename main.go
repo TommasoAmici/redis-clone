6216235c5beeeecda7a3f741fccb17f15cb4ddf4
Tommaso Amici <me@tommasoamici.com>
2022-03-27 20:43:38 +0200

    feat: added PING command

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -29,6 +31,9 @@ func main() {
 	}
 }
 
+var handlers = map[string]func(conn net.Conn, message string, args []string){
+	"ping":   Ping,
+}
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
@@ -40,6 +45,33 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 		log.Println("[INFO] Message Received:", msg)
+
+		msg = strings.TrimSpace(msg)
+		split := strings.Split(msg, " ")
+		command := strings.ToLower(split[0])
+		args := split[1:]
+
+		handler, ok := handlers[command]
+		if !ok {
+			return
+		}
 		handler(conn, msg, args)
 	}
+}
+
+// Ping returns PONG if no argument is provided, otherwise return a copy of the argument as a bulk.
+// This command is often used to test if a connection is still alive, or to measure latency.
+// https://redis.io/commands/ping/
+func Ping(conn net.Conn, msg string, args []string) {
+	if len(args) == 0 {
+		conn.Write([]byte("PONG\n"))
+	} else if len(args) == 1 {
+		conn.Write([]byte(args[0] + "\n"))
+	} else {
+		wrongNumberArgs(conn, "ping")
+	}
+}
+
+func wrongNumberArgs(conn net.Conn, name string) {
+	conn.Write([]byte("ERR wrong number of arguments for '" + name + "' command\n"))
 }
