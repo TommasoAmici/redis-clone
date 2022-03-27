e579814bfb8b1cecfb1206bf4678ca7d55a463ab
Tommaso Amici <me@tommasoamici.com>
2022-03-27 20:42:30 +0200

    fix: accept network connections and echo to console

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ BUILD=main
 
 build:
 	go build -o ${BUILD} main.go
+
+run: build
+	./${BUILD} -address 127.0.0.1:6380
--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"log"
+	"net"
+)
+
 func main() {
+	network := flag.String("network", "tcp", `The network must be "tcp", "tcp4", "tcp6", "unix" or "unixpacket".`)
+	addr := flag.String("address", "127.0.0.1:6379", "Address to listen on")
+	flag.Parse()
+
+	ln, err := net.Listen(*network, *addr)
+	if err != nil {
+		log.Fatalln("[ERROR] Failed to start listening on", *addr)
+	} else {
+		log.Println("[INFO] Listening on", *addr)
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("[ERROR]", err)
+		}
+		go handleConnection(conn)
+	}
 }
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil || msg == "" {
+			return
+		}
+		log.Println("[INFO] Message Received:", msg)
+		handler(conn, msg, args)
+	}
+}
